5a40654dece7ccfbe71c94e2638b02758e02b1cc
dfe <2211054370abc@gmail.com>
2025-03-05 11:15:58 +1100

    updated examples

--- src/mtbacktest/examples.py
+++ src/mtbacktest/examples.py
@@ -1,8 +1,4 @@
 from strategy import Strategy
-from lib.preprocessing import df_to_dict, data_preprocess
-import pandas as pd
-import numpy as np
-
 class DummyStrat():
     def __init__(self):
         self.trader = Strategy()
@@ -41,9 +37,14 @@ class DummyStrat():
             self.trader.close_position(data['timestamp'], 'AAPL', data['close_AAPL'])
 class MultiTickerDummyStrat():
     def __init__(self):
-        self.trader = Strategy()
+        self.trader = Strategy() # A virtual trader that you can submit orders and contains information about the portfolio it manages, you MUST DEFINE THIS AS 'self.trader'
         self.account = self.trader.account
         def signal(dreturn):
+            """
+            Define the signal such that if the current day return is greater than 2% we open long position,
+            and vice versa, this is performed on all tickers passed into the iter function, which runs, 1 iteration
+            of the algorithm
+            """
             if dreturn > 2:
                 return 1
             if dreturn < -2:
@@ -51,27 +52,35 @@ class MultiTickerDummyStrat():
             return 0
         self.signal_func= signal
     
-    def iter(self, data, ticker):
-        curr_signal = self.signal_func(data['dreturn_' + ticker])
-        units = (self.account.buying_power // 3)/data['close_' + ticker]
-        curr_portfolio = self.account.get_curr_portfolio()
-        open_positions = [pos for pos in curr_portfolio.positions if pos.symbol == ticker and pos.status == 'open']
-        if curr_signal == 1:
-            '''
-            We long equity
-            '''
-            if len(open_positions) == 0:
-                self.trader.create_position(data['timestamp'], ticker, units, data['close_'+ticker])
-        
-        elif curr_signal == -1:
-            '''
-            We short equity
-            '''
-            if len(open_positions) == 0:
-                self.trader.create_position(data['timestamp'], ticker, -units, data['close_'+ticker])
+    def iter(self, data, tickers):
+        """
+        You must define an iter() method that takes in data and specify what you want to do each iteration
+        Note: you can design a different strategy for each ticker, for more customisable strategies.
+        """
 
-        elif curr_signal == 0 and len(open_positions) > 0:
-            '''
-            We close position
-            '''
-            self.trader.close_position(data['timestamp'], ticker, data['close_'+ticker])
+        for ticker in tickers:
+
+            data['dreturn_' + ticker] = (data['close_' + ticker] - data['open_' + ticker]) / data['open_' + ticker] * 100
+            curr_signal = self.signal_func(data['dreturn_' + ticker])
+            units = (self.account.buying_power // 3)/data['close_' + ticker]
+            curr_portfolio = self.account.portfolio_snapshots.iloc[-1]['portfolio']
+            open_positions = [pos for pos in curr_portfolio.positions if pos.symbol == ticker and pos.status == 'open']
+            if curr_signal == 1:
+                '''
+                We long equity
+                '''
+                if len(open_positions) == 0:
+                    self.trader.create_position(data['timestamp'], ticker, units, data['close_'+ticker])
+            
+            elif curr_signal == -1:
+                '''
+                We short equity
+                '''
+                if len(open_positions) == 0:
+                    self.trader.create_position(data['timestamp'], ticker, -units, data['close_'+ticker])
+
+            elif curr_signal == 0 and len(open_positions) > 0:
+                '''
+                We close position
+                '''
+                self.trader.close_position(data['timestamp'], ticker, data['close_'+ticker])
